98225c4898fc98528d17c6be11fb9a2cb30ab898
Verônica Souza <veronicasp54@gmail.com>
2020-01-23 23:26:29 -0300

    Atualizações de layout

---
+++ ProjetoControleSalas/app/src/main/java/com/example/myapplication/activity/projetocontrolesalas/ui/LoginEmpresa.java
@@ -0,0 +1,16 @@
+package com.example.myapplication.activity.projetocontrolesalas.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.activity.projetocontrolesalas.R;
+
+public class LoginEmpresa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_empresa);
+    }
+}
