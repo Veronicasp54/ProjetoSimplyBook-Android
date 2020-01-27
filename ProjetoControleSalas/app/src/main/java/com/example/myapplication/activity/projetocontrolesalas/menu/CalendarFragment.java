d734fe986a2b344d5dbe73e7161451dca9795b35
Veronica Souza <veronicasp54@gmail.com>
2020-01-27 12:19:49 -0300

    Implementando Calendario

--- ProjetoControleSalas/app/src/main/java/com/example/myapplication/activity/projetocontrolesalas/menu/CalendarFragment.java
+++ ProjetoControleSalas/app/src/main/java/com/example/myapplication/activity/projetocontrolesalas/menu/CalendarFragment.java
@@ -1,6 +1,8 @@
 package com.example.myapplication.activity.projetocontrolesalas.menu;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,19 +15,56 @@ import androidx.fragment.app.Fragment;
 
 import com.example.myapplication.activity.projetocontrolesalas.R;
 
+import java.util.Calendar;
+
+import devs.mulham.horizontalcalendar.HorizontalCalendar;
+import devs.mulham.horizontalcalendar.utils.HorizontalCalendarListener;
+
 public class CalendarFragment extends Fragment {
 
     private CalendarView calendarView;
     private TextView textViewDataAtual;
+    private HorizontalCalendar horizontalCalendar;
+    private View view;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
+        view = inflater.inflate(R.layout.fragment_calendar, container, false);
+        // Inflate the layout for this fragment
+
+        /* start before 1 month from now */
+        Calendar startDate = Calendar.getInstance();
+        startDate.add(Calendar.MONTH, -1);
+
+        /* end after 1 month from now */
+        Calendar endDate = Calendar.getInstance();
+        endDate.add(Calendar.MONTH, 1);
+
+        horizontalCalendar = new HorizontalCalendar.Builder(view, R.id.calendarView)
+                .range(startDate, endDate)
+                .datesNumberOnScreen(5)
+                .configure()
+                .formatTopText("MMM")
+                .formatMiddleText("dd")
+                .formatBottomText("EEE")
+                .textSize(14f, 24f, 14f)
+                .showTopText(true)
+                .showBottomText(true)
+                .textColor(Color.GRAY,Color.BLACK)
+                .end()
+                .build();
+
+        horizontalCalendar.setCalendarListener(new HorizontalCalendarListener() {
+            @Override
+            public void onDateSelected(Calendar date, int position) {
+                Toast.makeText(getContext(), DateFormat.format("EEE, MMM d, yyyy", date) + " is selected!", Toast.LENGTH_SHORT).show();
+            }
+
+        });
 
 
-
-
+        return view;
     }
 
 
