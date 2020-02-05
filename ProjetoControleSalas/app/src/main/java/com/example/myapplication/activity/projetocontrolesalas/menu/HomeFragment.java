f415120e6feb02f67e4def971388ee394f1b477c
Veronica Souza <veronicasp54@gmail.com>
2020-02-05 11:17:43 -0300

    Criando layouts de interação para reserva de salas

--- ProjetoControleSalas/app/src/main/java/com/example/myapplication/activity/projetocontrolesalas/menu/HomeFragment.java
+++ ProjetoControleSalas/app/src/main/java/com/example/myapplication/activity/projetocontrolesalas/menu/HomeFragment.java
@@ -1,5 +1,7 @@
 package com.example.myapplication.activity.projetocontrolesalas.menu;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,17 +23,14 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-         view = inflater.inflate(R.layout.fragment_home, container, false);
-
-
-
+        view = inflater.inflate(R.layout.fragment_home, container, false);
 
 
         return view;
 
     }
 
-    public void iniciaCampos(){
+    public void iniciaCampos() {
         textNomeEmpresa = view.findViewById(R.id.textViewNomeEmpresa);
         listSalas = view.findViewById(R.id.lista_salas_listview);
 
@@ -42,6 +41,27 @@ public class HomeFragment extends Fragment {
     private void listDetails() {
 
 
+    }
+
+    private void showDialogDetalhes() {
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+
+        builder.setView(inflater.inflate(R.layout.box_reservar_sala, null));
+
+        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+
+            }
+        });
+
+        AlertDialog dialog = builder.create();
     }
 
 
