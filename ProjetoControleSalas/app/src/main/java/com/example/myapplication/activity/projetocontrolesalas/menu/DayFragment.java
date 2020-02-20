0da180bcd134dcf9e6f73a7e90e23d65d07f1921
Veronica Souza <veronicasp54@gmail.com>
2020-02-20 14:48:37 -0300

    Implementacao de Reservas por usuario

--- ProjetoControleSalas/app/src/main/java/com/example/myapplication/activity/projetocontrolesalas/menu/DayFragment.java
+++ ProjetoControleSalas/app/src/main/java/com/example/myapplication/activity/projetocontrolesalas/menu/DayFragment.java
@@ -1,25 +1,29 @@
 package com.example.myapplication.activity.projetocontrolesalas.menu;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.myapplication.activity.projetocontrolesalas.R;
+import com.example.myapplication.activity.projetocontrolesalas.model.ReservaSala;
 import com.example.myapplication.activity.projetocontrolesalas.model.Sala;
 import com.example.myapplication.activity.projetocontrolesalas.services.RequestExibirReservas;
-import com.example.myapplication.activity.projetocontrolesalas.services.RequestSalas;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
@@ -30,6 +34,10 @@ public class DayFragment extends Fragment {
     private View view;
     private TextView dataAtual, quantReunioes;
 
+    private List<ReservaSala> reservas = new ArrayList<>();
+    private ArrayAdapter<String> adapter;
+    private ListView listRerservas;
+    private List<String> itemReserva = new ArrayList<>();
     private SharedPreferences preferences;
     public static final String userPreferences = "userPreferences";
 
@@ -49,7 +57,13 @@ public class DayFragment extends Fragment {
         dataAtual = view.findViewById(R.id.dataAtual);
         dataAtual.setText(getData());
 
+        listRerservas = view.findViewById(R.id.listReservas);
+
         quantReunioes = view.findViewById(R.id.textQuantReunioes);
+
+
+        exibirReservas();
+
 
     }
 
@@ -63,9 +77,14 @@ public class DayFragment extends Fragment {
 
     private void exibirReservas() {
 
+        
+
+        preferences = getActivity().getSharedPreferences(userPreferences, Context.MODE_PRIVATE);
+        System.out.println(preferences.getString("userId", null));
+
         String requestReservas = null;
         try {
-            requestReservas = new RequestExibirReservas().execute(preferences.getString("id_usuario", null)).get();
+            requestReservas = new RequestExibirReservas().execute(preferences.getString("userId", null)).get();
 
             System.out.println(requestReservas);
 
@@ -74,11 +93,33 @@ public class DayFragment extends Fragment {
             if (reservasJson.length() > 0) {
 
                 for (int i = 0; i < reservasJson.length(); i++) {
-                    JSONObject reservaJson = reservasJson.getJSONObject(i);
 
+                    JSONObject jsonObjectReserva = reservasJson.getJSONObject(i);
 
+                    if (jsonObjectReserva.has("idUsuario") && jsonObjectReserva.has("id") ) {
+
+                        //I/System.out: [{"ativo":true,"dataAlteracao":"2020-02-19T17:00:15Z[UTC]","dataCriacao":"2020-02-19T17:00:15Z[UTC]","dataHoraFim":"2020-02-19T20:00:00Z[UTC]","dataHoraInicio":"2020-02-19T21:00:00Z[UTC]","descricao":"reserva","id":1,"idSala":1,"idUsuario":7,"nomeOrganizador":"Verônica Souza"},{"ativo":true,"dataAlteracao":"2020-02-19T17:01:54Z[UTC]","dataCriacao":"2020-02-19T17:01:54Z[UTC]","dataHoraFim":"2020-02-19T21:01:00Z[UTC]","dataHoraInicio":"2020-02-19T22:01:00Z[UTC]","descricao":"reserva","id":2,"idSala":1,"idUsuario":7,"nomeOrganizador":"Verônica Souza"},{"ativo":true,"dataAlteracao":"2020-02-19T17:04:09Z[UTC]","dataCriacao":"2020-02-19T17:04:09Z[UTC]","dataHoraFim":"2020-02-19T20:04:00Z[UTC]","dataHoraInicio":"2020-02-19T21:04:00Z[UTC]","descricao":"re","id":3,"idSala":1,"idUsuario":7,"nomeOrganizador":"Verônica Souza"},{"ativo":true,"dataAlteracao":"2020-02-19T17:07:15Z[UTC]","dataCriacao":"2020-02-19T17:07:15Z[UTC]","dataHoraFim":"2020-02-19T20:07:00Z[UTC]","dataHoraInicio":"2020-02-19T21:07:00Z[UTC]","descricao":"reservar","id":4,"idSala":1,"idUsuario":7,"nomeOrganizador":"Verônica Souza"},{"ativo":true,"dataAlteracao":"2020-02-20T14:16:57Z[UTC]","dataCriacao":"2020-02-20T14:16:57Z[UTC]","dataHoraFim":"2020-02-20T17:16:00Z[UTC]","dataHoraInicio":"2020-02-20T17:30:00Z[UTC]","descricao":"desc","id":5,"idSala":1,"idUsuario":7,"nomeOrganizador":"Verônica Souza"}]
+                        int idUser = jsonObjectReserva.getInt("idUsuario");
+                        int idSala = jsonObjectReserva.getInt("idSala");
+                        int idReserva = jsonObjectReserva.getInt("id");
+                        String descricaoReserva = jsonObjectReserva.getString("descricao");
+                        String dataHoraInicio = jsonObjectReserva.getString("dataHoraInicio");
+                        String dataHoraFim = jsonObjectReserva.getString("dataHoraFim");
+
+                        ReservaSala newReserva = new ReservaSala();
+
+                        newReserva.setIdSala(idSala);
+                        newReserva.setDescricaoReserva(descricaoReserva);
+                        newReserva.setIdUser(idUser);
+
+                        reservas.add(newReserva);
+                        itemReserva.add(descricaoReserva);
+                    }
 
                 }
+                listRerservas = view.findViewById(R.id.listReservas);
+                adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, itemReserva);
+                listRerservas.setAdapter(adapter);
 
             }
 
